15d572da6bbba8a18d79f9aafeb06344cda85512
fatima <fbazroun@asu.edu>
2025-04-30 12:15:56 +0300

    this is for the deployment

--- Backend/backend.js
+++
@@ -1,8 +0,0 @@
-/*const bcrypt = require('bcryptjs');
-
-async function hashPassword(password) {
-  const hashed = await bcrypt.hash(password, 10);
-  console.log('Hashed Password:', hashed);
-}
-
-hashPassword("Fatima@123"); */
